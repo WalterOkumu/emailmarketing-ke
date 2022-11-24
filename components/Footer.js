6559c41519934c216d7035cb67bcc557cdedd31b
Helder Fonseca <helder2391@gmail.com>
2022-11-24 17:04:16 -0100

    change Footer component

--- components/Footer.js
+++ components/Footer.js
@@ -3,14 +3,14 @@ import { FaEnvelope, FaPhoneAlt } from 'react-icons/fa';
 export default function Footer() {
   return (
     <section className="mt-16 bg-[#181818]">
-      <div className="grid md:grid-cols-3 text-[#f8f8ff] py-16 md:p-12 m-auto gap-14 md:gap-16 max-w-5xl place-items-center">
-        <div className="text-left text-sm">
+      <div className="grid md:grid-cols-3 text-[#f8f8ff] py-16 md:p-12 mx-auto gap-14 md:gap-16 max-w-5xl place-items-center">
+        <div className="text-left text-sm w-1/2 md:w-11/12">
           <p className="text-2xl mb-3">Address</p>
           <p className='text-gray-300'>
             Muthithi Road, Mitsumi Business Park, 8th Floor Nairóbi, Quénia 00100
           </p>
         </div>
-        <div className="text-sm text-left">
+        <div className="text-sm text-left w-1/2 md:w-11/12">
           <p className="text-2xl mb-3">Email</p>
           <div className='flex items-center gap-2'>
             <FaEnvelope />
@@ -22,11 +22,11 @@ export default function Footer() {
             <p className="text-gray-300">Tel: +254 020 7800 701</p>
   </div>*/}
         </div>
-        <div className="text-left text-sm">
-          <p className="text-2xl mb-3">Phones</p>
-          <div className='flex items-center justify-start gap-2'>
+        <div className="text-left text-sm w-1/2 md:w-11/12">
+          <p className="text-2xl items-center mb-3">Phones</p>
+          <div className='flex gap-2'>
             <FaPhoneAlt />
-            <p className="text-gray-300">Tel: <a href='tel:+254 020 7800 701'>+254 020 7800 701</a></p>
+            <p className="text-gray-300 self-start">Tel: <a href='tel:+254 020 7800 701'>+254 020 7800 701</a></p>
           </div>
         </div>
       </div>
